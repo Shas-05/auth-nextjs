3dc57be3bcc8d8628588860929defe0932c93683
Shas-05 <shashank.cms.2005@gmail.com>
2026-03-06 21:33:06 +0530

    logout updated

---
+++ src/app/api/user/logout/route.ts
@@ -0,0 +1,16 @@
+import next from "next";
+import { NextResponse } from "next/server";
+
+export async function GET(){
+    try{
+        const response = NextResponse.json({
+            message: "Logout successful",
+            success:true,
+        })
+        response.cookies.set("token", "", {httpOnly:true, expires: new Date(0)});
+        return response;
+    }catch(error:any){
+        return NextResponse.json({ message: "Logout failed" }, { status: 500 });
+    }
+
+}
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,9 +1,32 @@
+"use client";
+import React from "react";
+import axios from "axios";
+import links from "next/link";
+import { useRouter } from "next/navigation";
+
 export default function ProfilePage() {
-    return (
-        <div className="flex flex-col items-center justify-center min-h-screen py-2">
-            <h1>Profile</h1>
-            <hr />
-            <p>Welcome to your profile page!</p>
-        </div>
-    );
-}   
+  const router = useRouter();
+  const logout = async () => {
+    try {
+      await axios.get("/api/user/logout");
+      router.push("/login");
+    } catch (error: any) {
+      console.log("Logout failed", error.message);
+    }
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1>Profile</h1>
+      <hr />
+      <p>Welcome to your profile page!</p>
+      <hr />
+      <button
+        onClick={logout}
+        className="mt-4 rounded-lg bg-red-500 px-4 py-2 text-sm font-medium text-white hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-200"
+      >
+        Logout
+      </button>
+    </div>
+  );
+}
